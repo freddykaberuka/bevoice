9ae76b55790ec9318fa5b8ea2c62748839e20734
Freddy KABERUKA <freddykaberuka2009@gmail.com>
2022-05-23 15:44:20 -0700

    setup new page

--- bevoice/src/App.js
+++ bevoice/src/App.js
@@ -2,10 +2,12 @@ import './App.scss';
 import React from 'react';
 import Home from './component/landing/home';
 import './App.scss'
+import About from './component/about/about';
 function App() {
   return (
     <div className="App">
-      <Home/>
+      {/* <Home/> */}
+      <About/>
     </div>
   );
 }
---
+++ bevoice/src/component/about/about.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Slider from '../landing/slider'
+
+function About() {
+  return (
+    <div><Slider/></div>
+  )
+}
+
+export default About
