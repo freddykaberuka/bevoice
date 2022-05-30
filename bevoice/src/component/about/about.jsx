9ddee6c69bb6f3dfa214d89c3463b952e2ffd1fb
Freddy KABERUKA <freddykaberuka2009@gmail.com>
2022-05-30 06:05:24 -0700

    add approach section

--- bevoice/src/component/about/about.jsx
+++ bevoice/src/component/about/about.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Slider from '../landing/slider'
 import History from './history'
 import './about.scss'
+import Aproach from './aproach'
 
 function About() {
   return (
     <div>
         <Slider/>
         <History/>
+        <Aproach/>
     </div>
   )
 }
---
+++ bevoice/src/component/about/aproach.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Aproach() {
+  return (
+    <div>
+        <h1>OUR APPROACH</h1>
+        <div className="approach-container">
+            
+        </div>
+    </div>
+  )
+}
+
+export default Aproach
