aaaadf6cd2cddd9285a2e9dde14a7a0a824f72e9
Freddy KABERUKA <freddykaberuka2009@gmail.com>
2022-06-01 14:12:08 -0700

    add footer

--- bevoice/src/App.js
+++ bevoice/src/App.js
@@ -3,11 +3,13 @@ import React from 'react';
 import Home from './component/landing/home';
 import './App.scss'
 import About from './component/about/about';
+import Footer from './component/footer';
 function App() {
   return (
     <div className="App">
       {/* <Home/> */}
       <About/>
+      <Footer/>
     </div>
   );
 }
