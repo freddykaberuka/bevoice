6fee483c2a04e2d45cddc15a574bc24ed322f65b
Freddy KABERUKA <freddykaberuka2009@gmail.com>
2022-06-04 02:49:19 -0700

    add approach section

--- bevoice/src/component/about/aproach.js
+++ bevoice/src/component/about/aproach.js
@@ -2,10 +2,53 @@ import React from 'react'
 
 function Aproach() {
   return (
-    <div>
+    <div className='approach'>
         <h1>OUR APPROACH</h1>
         <div className="approach-container">
-            
+            <div className="missions">
+              <div>
+                <h3>— Our Mission</h3>
+                <p>The mission of Rwanda We Want Organization is to empower the youth and bring them together 
+                  to become the pillars of sustainable development"
+                </p>
+              </div>
+              <div>
+                <h3>— Our Vision</h3>
+                <p>Having a critical thinking youth capable of designing and putting in place policies leading 
+                  to sustainable peace and development
+                </p>
+              </div>
+              <div>
+                <h3>— Areas Of Focus</h3>
+                <ul>
+                  <li>Youth Empowerment</li>
+                  <li>Youth Sexual Reproductive Health</li>
+                  <li>Civic Education</li>
+                  <li>Inclusion</li>
+                </ul>
+              </div>
+            </div>
+            <div className="projects">
+              <div>
+                <h4>Rwanda We Want Organization follows the below principles in the implementation of its projects:</h4>
+                <ul>
+                  <li>Ensuring a youth-friendly environment</li>
+                  <li>Use of youth-friendly learning models</li>
+                  <li>Promoting a culture of innovation and creativity</li>
+                  <li>Making efforts to achieve the highest quality standards</li>
+                </ul>
+              </div>
+              <div>
+                <h3>— Our Current Projects</h3>
+                <ul>
+                  <li>IKIRARO</li>
+                  <li>IKIRARO</li>
+                  <li>IKIRARO</li>
+                  <li>IKIRARO</li>
+                </ul>
+              </div>
+              <button>LEARN MORE</button>
+            </div>
         </div>
     </div>
   )
