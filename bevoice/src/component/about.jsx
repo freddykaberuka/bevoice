7c8f0db7324dfa91e4014c5cf0f4771672176df0
Freddy KABERUKA <freddykaberuka2009@gmail.com>
2022-06-16 16:06:31 -0700

    add about page

--- bevoice/src/component/about.jsx
+++ bevoice/src/component/about.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import image1 from '../assets/1.jpg'
 import History from './about/history'
+import arsen from '../assets/ars.jpg'
+import fred from '../assets/arsen.jpg'
 function AboutPage() {
   return (
     <div>
@@ -25,40 +27,13 @@ function AboutPage() {
         <section id="video-sec">
                 <div className="container">
                 <div className="row text-center">
-                <h1>How can you help?</h1>
+                <h1>AT OUR OFFICE</h1>
                 <hr/>
-                <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h5>
                 <div className="text-left"> 
-                <div className="col-md-6 clearfix top-off">
+                <div className="col-md-12 clearfix top-off">
                 <video width="400" controls>
                 <source src="images/mov_bbb.mp4" type="video/mp4"/>
                 </video>
-                </div>
-                <div className="col-md-6 clearfix top-off">
-                <div className="media">
-                <div className="media-image">
-                <img src="images/g1.png" className="attachment-full size-full" alt="g1"/>																							</div>
-                <div className="media-text">
-                <h5>BECOME A VOLUNTEER</h5>
-                <p>Give us a brief description of the service that you are promoting.</p>
-                </div>
-                </div>
-                <div className="media">
-                <div className="media-image">
-                <img src="images/g2.png" className="attachment-full size-full" alt="g1"/>																							</div>
-                <div className="media-text">
-                <h5>MAKE A GIFT</h5>
-                <p>Give us a brief description of the service that you are promoting.</p>
-                </div>
-                </div>
-                <div className="media">
-                <div className="media-image">
-                <img src="images/g1.png" className="attachment-full size-full" alt="g1"/>																							</div>
-                <div className="media-text">
-                <h5>GIVE A SCHOLASHIP</h5>
-                <p>Give us a brief description of the service that you are promoting.</p>
-                </div>
-                </div>
                 </div>
                 </div>
                 </div>
@@ -72,33 +47,29 @@ function AboutPage() {
             <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h5>
             <div class="text-left"> 
             <div class="col-md-6 clearfix top-off">
-            <div class="icon_circle"><i class="fa fa-heartbeat">&nbsp;</i></div>
             <div class="help-text">
-            <h4>Donate</h4>
+            <h4>Our Mission</h4>
             <div class="line line-50"></div>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
             </div>
             <div class="col-md-6 clearfix top-off">
-            <div class="icon_circle"><i class="fa fa-user-plus">&nbsp;</i></div>
             <div class="help-text">
-            <h4>Participate</h4>
+            <h4>Our Vision</h4>
             <div class="line line-50"></div>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
             </div>
             <div class="col-md-6 clearfix top-off">
-            <div class="icon_circle"><i class="fa fa-usd">&nbsp;</i></div>
             <div class="help-text">
-            <h4>Fundraise</h4>
+            <h4>Area of focus</h4>
             <div class="line line-50"></div>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
             </div>
             <div class="col-md-6 clearfix top-off">
-            <div class="icon_circle"><i class="fa fa-credit-card">&nbsp;</i></div>
             <div class="help-text">
-            <h4>Contribute</h4>
+            <h4>Our Current Projects</h4>
             <div class="line line-50"></div>
             <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
             </div>
@@ -115,57 +86,57 @@ function AboutPage() {
             <h5>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</h5>
             <div class="member col-xs-6 col-md-3">
             <div class="inner">
-            <div class="avatar-inner"><img src="images/team1.jpg"/></div>
-            <div class="info"><div class="name"><a href="#"> Michel Black </a></div><div class="regency">Volunteer</div></div>
+            <div class="avatar-inner"><img src={fred}/></div>
+            <div class="info"><div class="name"><a href="#"> Ishimwe Arsene</a></div><div class="regency"> CEO&FOUNDER</div></div>
             </div>
             </div>
 
             <div class="member col-xs-6 col-md-3">
             <div class="inner">
-            <div class="avatar-inner"><img src="images/team2.jpg"/></div>
-            <div class="info"><div class="name"><a href="#"> Juliana Moor </a></div><div class="regency">Volunteer</div></div>
+            <div class="avatar-inner"><img src={fred}/></div>
+            <div class="info"><div class="name"><a href="#"> Ishimwe Arsene</a></div><div class="regency"> CEO&FOUNDER</div></div>
             </div>
             </div>
 
             <div class="member col-xs-6 col-md-3">
             <div class="inner">
-            <div class="avatar-inner"><img src="images/team3.jpg"/></div>
-            <div class="info"><div class="name"><a href="#"> Andre Russel</a></div><div class="regency">Volunteer</div></div>
+            <div class="avatar-inner"><img src={fred}/></div>
+            <div class="info"><div class="name"><a href="#"> Ishimwe Arsene</a></div><div class="regency"> CEO&FOUNDER</div></div>
             </div>
             </div>
 
             <div class="member col-xs-6 col-md-3">
             <div class="inner">
-            <div class="avatar-inner"><img src="images/team4.jpg"/></div>
-            <div class="info"><div class="name"><a href="#"> Amily James</a></div><div class="regency">Volunteer</div></div>
+            <div class="avatar-inner"><img src={fred}/></div>
+            <div class="info"><div class="name"><a href="#"> Ishimwe Arsene</a></div><div class="regency"> CEO&FOUNDER</div></div>
             </div>
             </div>
 
             <div class="member col-xs-6 col-md-3">
             <div class="inner">
-            <div class="avatar-inner"><img src="images/team1.jpg"/></div>
-            <div class="info"><div class="name"><a href="#"> Michel Black </a></div><div class="regency">Volunteer</div></div>
+            <div class="avatar-inner"><img src={fred}/></div>
+            <div class="info"><div class="name"><a href="#"> Ishimwe Arsene</a></div><div class="regency"> CEO&FOUNDER</div></div>
             </div>
             </div>
 
             <div class="member col-xs-6 col-md-3">
             <div class="inner">
-            <div class="avatar-inner"><img src="images/team2.jpg"/></div>
-            <div class="info"><div class="name"><a href="#"> Juliana Moor </a></div><div class="regency">Volunteer</div></div>
+            <div class="avatar-inner"><img src={fred}/></div>
+            <div class="info"><div class="name"><a href="#"> Ishimwe Arsene</a></div><div class="regency"> CEO&FOUNDER</div></div>
             </div>
             </div>
 
             <div class="member col-xs-6 col-md-3">
             <div class="inner">
-            <div class="avatar-inner"><img src="images/team3.jpg"/></div>
-            <div class="info"><div class="name"><a href="#"> Andre Russel</a></div><div class="regency">Volunteer</div></div>
+            <div class="avatar-inner"><img src={fred}/></div>
+            <div class="info"><div class="name"><a href="#"> Ishimwe Arsene</a></div><div class="regency"> CEO&FOUNDER</div></div>
             </div>
             </div>
 
             <div class="member col-xs-6 col-md-3">
             <div class="inner">
-            <div class="avatar-inner"><img src="images/team4.jpg"/></div>
-            <div class="info"><div class="name"><a href="#"> Amily James</a></div><div class="regency">Volunteer</div></div>
+            <div class="avatar-inner"><img src={fred}/></div>
+            <div class="info"><div class="name"><a href="#"> Ishimwe Arsene</a></div><div class="regency"> CEO&FOUNDER</div></div>
             </div>
             </div>
 
@@ -181,7 +152,7 @@ function AboutPage() {
                         
                         <div class="col-md-6">
                             <div class="callout-actions">
-                                <a href="contact.html" class="button">Become Volunteer</a>
+                                <a href="contact.html" class="button">Become a Member</a>
 
                                 <span class="callout-separator">
                                     <span>Or</span>
